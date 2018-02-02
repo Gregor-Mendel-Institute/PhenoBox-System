78d590a029c3c1770a0ef44bbaa909259bd6b1eb
Sebastian Seitner <sebastian@syt-tech.at>
2018-02-02 11:55:58 +0100

    [phenopipe-iap-server] Added IAP password encryption option

--- phenopipe-iap-server/src/main/java/at/gmi/djamei/phenopipe/Phenopipe.java
+++ phenopipe-iap-server/src/main/java/at/gmi/djamei/phenopipe/Phenopipe.java
@@ -3,6 +3,14 @@ package at.gmi.djamei.phenopipe;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+import org.SystemOptions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,6 +26,7 @@ import at.gmi.djamei.mongo.PhenopipeIapMongoDb;
 public class Phenopipe {
 	static final Logger logger = LogManager.getLogger();
 	public static void main(String[] args) {
+		if(args.length==0){
 		logger.info("Starting up Phenopipe IAP Server");
 		Config.INSTANCE.load();
 		
@@ -45,6 +54,23 @@ public class Phenopipe {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		}else if (args.length==2 && args[0].equals("-encrypt-pw")){
+			SystemOptions so = SystemOptions.getInstance("secret", null);
+			String defaultValue = java.util.UUID.randomUUID().toString();
+			String passphrase = so.getString("Symmetric Encryption", "key", defaultValue);
+			DESKeySpec keySpec;
+			try {
+				keySpec = new DESKeySpec(passphrase.getBytes("UTF8"));
+				SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+				SecretKey key = keyFactory.generateSecret(keySpec);
+				Cipher encCipher = Cipher.getInstance("DES"); // cipher is not thread safe
+				encCipher.init(Cipher.ENCRYPT_MODE, key);
+				String res = Base64.getEncoder().encodeToString(encCipher.doFinal(args[1].getBytes("UTF-8")));
+				System.out.println("encrypted:"+res);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
